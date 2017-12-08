1590da5aeb9242a3004c0b74a2eb6f08904457f8
何建钦 <1373883296@qq.com>
2017-12-08 22:31:02 +0800

    tttt
    ssss

--- app/src/main/java/com/example/loveb/fds/MainActivity.java
+++ app/src/main/java/com/example/loveb/fds/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.loveb.fds;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +17,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Context mContext;
+    private GridView grid_video;
+    private FragmentPagerAdapter mAdapter = null;
+    private ArrayList<Video> mData = null;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +45,17 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ViewFlipper vflp_help = (ViewFlipper) findViewById(R.id.vF_main);
+        vflp_help.startFlipping();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +65,56 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+       // String str="http://www.runoob.com";
+
+//        URL url= new URL(str);
+        mViewPager = (ViewPager) findViewById(R.id.VP_video);
+//        grid_video = (GridView) findViewById(R.id.grid_video);
+
+        mContext=MainActivity.this;
+        mData = new ArrayList<Video>();
+        mData.add(new Video(R.mipmap.sss, "图标1"));
+        mData.add(new Video(R.mipmap.sss, "图标2"));
+        mData.add(new Video(R.mipmap.sss, "图标3"));
+        mData.add(new Video(R.mipmap.sss, "图标4"));
+        mData.add(new Video(R.mipmap.sss, "图标5"));
+        mData.add(new Video(R.mipmap.sss, "图标6"));
+        mData.add(new Video(R.mipmap.sss, "图标7"));
+
+//        mAdapter = new MyAdapter<Video>(mData, R.layout.video) {
+//            @Override
+//            public void bindView(ViewHolder holder, Video obj) {
+//                holder.setImageResource(R.id.img_icon, obj.getvId());
+//                holder.setText(R.id.txt_icon, obj.getvName());
+//            }
+//        };
+
+        String[] names = new String[]{"B神", "基神", "曹神"};
+        int[] imgIds = new int[]{R.mipmap.sss, R.mipmap.sss, R.mipmap.sss};
+        List<Map<String, Object>> listitem = new ArrayList<Map<String, Object>>();
+
+        for (int i = 0; i < names.length; i++) {
+            Map<String, Object> showitem = new HashMap<String, Object>();
+            showitem.put("touxiang", imgIds[i]);
+            showitem.put("name", names[i]);
+            listitem.add(showitem);
+        }
+        SimpleAdapter myAdapter = new SimpleAdapter(getApplicationContext(), listitem, R.layout.video, new String[]{"vId", "vName"}, new int[]{R.id.img_video, R.id.txt_video});
+
+
+        mViewPager.setAdapter(mAdapter);
+
+
+//        grid_video.setAdapter(mAdapter);
+//
+//        grid_video.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(mContext, "你点击了~" + position + "~项", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
     @Override
@@ -52,12 +127,13 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+//菜单
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -80,19 +156,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
